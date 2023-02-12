ea6f7a11b6b8747f38f72b5a11d125daad888ff1
abhayarya1709 <abhayarya2002@gmail.com>
2023-02-12 23:55:32 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -92,7 +92,7 @@ const handleSubmit = async (e) => {
 
   // bots response
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://e-ai.onrender.com/', {
     method:'POST',
     headers:{
       'Content-Type': 'application/json'
